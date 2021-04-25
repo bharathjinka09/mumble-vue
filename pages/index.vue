5800fc7112aa8f77e9c497358ea2c2a0dca6ad7c
ParveenMalethia <prince.malethia.36@gmail.com>
2021-04-25 19:51:16 +0530

    favicon updated

--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,12 @@
       </h1>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://mumble.dev"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
         >
-          GitHub
+          Website
         </a>
       </div>
     </div>
